bb23bf0bee67b952706e023e4c2e9f9ab81043fa
Joshua Ramirez <josh@Joshuas-MBP.fios-router.home>
2017-11-10 14:58:05 -0800

    Add user and singer models

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,36 @@
 package model;
 
 public class User {
+    private int userId;
+    private String password;
+    private String username;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("User(%s, %s, %s)", getUserId(), getUsername(), getPassword());
+    }
 }
