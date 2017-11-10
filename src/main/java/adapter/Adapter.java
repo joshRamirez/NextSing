b35706f682d7426431873d703cb3ea7d697c2302
Joshua Ramirez <josh@Joshuas-MBP.fios-router.home>
2017-11-10 15:27:29 -0800

    Add user and singer repository to adapter

--- src/main/java/adapter/Adapter.java
+++ src/main/java/adapter/Adapter.java
@@ -1,11 +1,17 @@
 package adapter;
 
 import repository.AlbumRepository;
+import repository.SingerRepository;
+import repository.UserRepository;
 
 public abstract class Adapter {
     AlbumRepository albumRepository;
+    SingerRepository singerRepository;
+    UserRepository userRepository;
 
-    public Adapter(AlbumRepository albumRepository){
+    public Adapter(AlbumRepository albumRepository, SingerRepository singerRepository, UserRepository userRepository){
         this.albumRepository = albumRepository;
+        this.singerRepository = singerRepository;
+        this.userRepository = userRepository;
     }
 }
